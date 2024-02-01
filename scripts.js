d8f0f1a9f4f886b8d41c2c54a9262c09bf9401dc
GreatDistances <117937385+GreatDistances@users.noreply.github.com>
2024-02-01 13:35:27 -0600

    Update: books can now be added to library

--- scripts.js
+++ scripts.js
@@ -4,28 +4,28 @@ const myLibrary = [
 ];
 
 const newBookBtn = document.getElementById("newBookBtn");
-newBookBtn.addEventListener("click", addBookToLibrary);
+newBookBtn.addEventListener("click", addNewBook);
 
 displayTable(myLibrary);
 
-
 function displayTable(myLibrary) {
-    const bookTable = document.createElement("table");
+  const tableContainer = document.getElementById("tableContainer");
+  tableContainer.innerText = "";
+  const bookTable = document.createElement("table");
+
+  const headerRow = document.createElement("tr");
+  const headerRowText = ["Author", "Title", "Pages", "Available"];
+
+  for (let i = 0; i < headerRowText.length; i++) {
+    let cell = document.createElement("td");
+    cell.innerText = headerRowText[i];
+    headerRow.append(cell);
+  }
+  bookTable.append(headerRow);
+
+  tableContainer.append(bookTable);
+
   if (myLibrary.length > 0) {
-    const tableContainer = document.getElementById("tableContainer");
-
-    const headerRow = document.createElement("tr");
-    const headerRowText = ["Author", "Title", "Pages", "Available"];
-
-    for (let i = 0; i < headerRowText.length; i++) {
-      let cell = document.createElement("td");
-      cell.innerText = headerRowText[i];
-      headerRow.append(cell);
-    }
-    bookTable.append(headerRow);
-
-    tableContainer.append(bookTable);
-
     for (i = 0; i < myLibrary.length; i++) {
       const thisBook = document.createElement("tr");
       let cell = document.createElement("td");
@@ -49,7 +49,6 @@ const newBookDialog = document.getElementById("newBookDialog");
 const newBookDialogCancelBtn = document.getElementById(
   "newBookDialogCancelBtn"
 );
-const submitNewBookBtn = document.getElementById("submitNewBookBtn");
 
 function Book(author, title, numPages) {
   this.author = author;
@@ -58,9 +57,31 @@ function Book(author, title, numPages) {
   this.isAvail = true;
 }
 
-function addBookToLibrary() {
-  console.log(myLibrary);
+function addNewBook() {
   newBookDialog.showModal();
+}
+
+const submitNewBookBtn = document.getElementById("submitNewBookBtn");
+submitNewBookBtn.addEventListener("click", submitNewBook);
+
+function submitNewBook(event) {
+  const author =
+    document.getElementById("newBookForm").elements["author"].value;
+  const title = document.getElementById("newBookForm").elements["title"].value;
+  const numPages =
+    document.getElementById("newBookForm").elements["numPages"].value;
+    if (author == "" || title == "" || numPages == "") {
+        alert("invalid data");
+    } else {
+  let book = new Book(author, title, numPages);
+  myLibrary.push(book);
+  displayTable(myLibrary); // refresh the table display to clear old table view
+  event.preventDefault;
+  document.getElementById("newBookForm").elements["author"].value = ""; // clear field on submit
+  document.getElementById("newBookForm").elements["title"].value = ""; // clear field on submit
+  document.getElementById("newBookForm").elements["numPages"].value = ""; // clear field on submit
+  newBookDialog.close(); // close newBookDialog window
+    }
 }
 
 newBookDialogCancelBtn.addEventListener("click", function () {
